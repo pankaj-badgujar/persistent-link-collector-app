2b3d9c907aac3f71c791f315adbd0ba61361fae5
pankajbadgujar <badgujar.p@husky.neu.edu>
2020-02-02 18:03:48 -0500

    implemented feature to add links

--- app/src/main/java/edu/neu/madcourse/numads20_pankajbadgujar/LinkCollector.java
+++ app/src/main/java/edu/neu/madcourse/numads20_pankajbadgujar/LinkCollector.java
@@ -1,16 +1,36 @@
 package edu.neu.madcourse.numads20_pankajbadgujar;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.IdRes;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
 
 public class LinkCollector extends AppCompatActivity {
+
+    public List<String> linkNameList = new ArrayList<>();
+    public Map<String,String> urlMap = new HashMap<>();
+    final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +39,66 @@ public class LinkCollector extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        ListView listView = findViewById(R.id.linkList);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, linkNameList);
+        listView.setAdapter(arrayAdapter);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                // get prompts.xml view
+                LayoutInflater layoutInflater = LayoutInflater.from(context);
+                final View parentView = view;
+                final View promptView = layoutInflater.inflate(R.layout.input_prompt, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                // set prompts.xml to be the layout file of the alertdialog builder
+                alertDialogBuilder.setView(promptView);
+
+                final EditText nameInput = (EditText) promptView.findViewById(R.id.linkNameInput);
+                final EditText urlInput = (EditText) promptView.findViewById(R.id.linkUrlInput);
+
+
+
+                // setup a dialog window
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // get user input and set it to result
+
+                                String name = nameInput.getText().toString();
+                                String url = urlInput.getText().toString();
+
+                                urlMap.put(name,url);
+                                linkNameList.add(name);
+
+                                Snackbar.make(parentView,name+" is added",Snackbar.LENGTH_LONG)
+                                        .setAction("Action", null).show();
+
+
+
+                            }
+                        })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create an alert dialog
+                AlertDialog alertD = alertDialogBuilder.create();
+
+                alertD.show();
+
+
+
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
