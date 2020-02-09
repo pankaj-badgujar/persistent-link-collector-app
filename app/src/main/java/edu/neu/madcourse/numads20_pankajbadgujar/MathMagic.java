36ec13ab4ea87d346b14e229b9b054035b40eeb7
pankajbadgujar <badgujar.p@husky.neu.edu>
2020-02-09 12:31:02 -0500

    completed math magic feature
    completed math magic feature

--- app/src/main/java/edu/neu/madcourse/numads20_pankajbadgujar/MathMagic.java
+++ app/src/main/java/edu/neu/madcourse/numads20_pankajbadgujar/MathMagic.java
@@ -6,13 +6,13 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 public class MathMagic extends AppCompatActivity {
 
@@ -20,6 +20,10 @@ public class MathMagic extends AppCompatActivity {
     private Button cancelSearchBtn;
     private AsyncTask findPrimesTask;
     private TextView currentStatusTxt;
+    protected ListView primesListView;
+    protected ArrayAdapter<Integer> arrayAdapter;
+    protected List<Integer> primes;
+    protected TextView primeNumbersFoundLabel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +40,19 @@ public class MathMagic extends AppCompatActivity {
         findPrimesBtn = (Button) findViewById(R.id.findPrimesBtn);
         cancelSearchBtn = (Button) findViewById(R.id.cancelSearchBtn);
         currentStatusTxt = (TextView) findViewById(R.id.currentStatusTxt);
+        primesListView = (ListView) findViewById(R.id.primesList);
+        primes = new ArrayList<>();
+        primeNumbersFoundLabel = (TextView) findViewById(R.id.primeNumbersFoundLabel);
 
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, primes);
+
+        primesListView.setAdapter(arrayAdapter);
 
         //keeping cancel prime search button invisible at the start
         cancelSearchBtn.setVisibility(View.INVISIBLE);
+
+        //keeping prime numbers found label hidden at the start
+        primeNumbersFoundLabel.setVisibility(View.INVISIBLE);
     }
 
     public void startFindPrimeTask(View view) {
@@ -67,31 +80,25 @@ public class MathMagic extends AppCompatActivity {
 
     private class FindPrimesTask extends AsyncTask<Object, Integer, String> {
 
-        private List<Integer> primes = new ArrayList<>();
-
         @Override
-        protected void onCancelled() {
-            currentStatusTxt.setText(R.string.searchEndedTxt);
+        protected void onPreExecute() {
+            primeNumbersFoundLabel.setVisibility(View.VISIBLE);
+            primes.clear();
         }
 
         @Override
         protected String doInBackground(Object... voids) {
-
             int current = 2;
-
-
             while (true) {
 
                 if (isCancelled()) {
-
                     break;
-
                 }
 
                 boolean isPrime = true;
                 publishProgress(current);
                 try {
-                    Thread.sleep(600);
+                    Thread.sleep(300);
                 } catch (InterruptedException e) {
 
                 }
@@ -111,19 +118,21 @@ public class MathMagic extends AppCompatActivity {
             return null;
         }
 
+
         @Override
         protected void onProgressUpdate(Integer... current) {
             updateCurrentStatus(current[0]);
+            primesListView.setAdapter(arrayAdapter);
         }
 
-        private void updateCurrentStatus(int number){
-            String sentence = getString(R.string.currentStatus,number);
+        private void updateCurrentStatus(int number) {
+            String sentence = getString(R.string.currentStatus, number);
             currentStatusTxt.setText(sentence);
         }
 
         @Override
-        protected void onPostExecute(String aVoid) {
-
+        protected void onCancelled() {
+            currentStatusTxt.setText(R.string.searchEndedTxt);
         }
     }
 
