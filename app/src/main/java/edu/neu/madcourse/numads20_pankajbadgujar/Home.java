ca311a4adad2eb2d42a8ee53dbcd13169973f2e6
pankajbadgujar <badgujar.p@husky.neu.edu>
2020-02-15 19:59:58 -0500

    created basic format of rendering
    created basic format of rendering

--- app/src/main/java/edu/neu/madcourse/numads20_pankajbadgujar/Home.java
+++ app/src/main/java/edu/neu/madcourse/numads20_pankajbadgujar/Home.java
@@ -1,19 +1,25 @@
 package edu.neu.madcourse.numads20_pankajbadgujar;
 
 import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
-import android.widget.Toast;
+
 
 public class Home extends AppCompatActivity {
 
+    private final Context context = this;
     private BroadcastReceiver receiver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +29,7 @@ public class Home extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         configureReceiver();
-
     }
-
 
     public void startAboutActivity(View view) {
         startActivity(new Intent(this, HelloWorld.class));
@@ -50,8 +54,29 @@ public class Home extends AppCompatActivity {
     }
 
     public void onLatitudeLongitudeButtonClick(View view){
-        Toast.makeText(this,"latitude",Toast.LENGTH_SHORT).show();
+
+        int[] values = calculateLatitudeLongitude();
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+        alertDialogBuilder
+                .setMessage(getString(R.string.latitude,values[0])+"\n\n"+
+                        getString(R.string.longitude,values[1]))
+                .setCancelable(false)
+                .setPositiveButton("Close", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        AlertDialog latitudeLongitudeDialog = alertDialogBuilder.create();
+        latitudeLongitudeDialog.show();
     }
+
+    private int[] calculateLatitudeLongitude() {
+        return new int[]{123,343};
+    }
+
 
     @Override
     protected void onDestroy() {
